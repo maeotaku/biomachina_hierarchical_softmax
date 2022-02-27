4bfccdef02fe3d548329492e974c0560df38270b
Kelvin Jimenez <kelvin@kelvin.cr>
2022-02-28 00:25:11 +0700

    function to get_mrr

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 def accuracy(output, target, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
@@ -15,3 +16,16 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
+
+def get_mrr(predictions_dict):
+    ranks = np.asarray([get_rank(value) for key, value in predictions_dict.items()])
+    return np.sum((1 / ranks)) / len(predictions_dict)
+
+def get_rank(dict_value):
+    prob = dict_value['prob']
+    label = dict_value['label']
+    idx = np.argsort(prob)[::-1]
+    np.argmax(prob) == label
+    rank_i = np.squeeze(np.where(idx == label)) + 1
+
+    return rank_i
