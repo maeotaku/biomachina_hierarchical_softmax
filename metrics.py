d3f99608067aceb1ed05cf4de0bf70201bd5a41e
rubengonzalez <rgonzalez@datasoftla.com>
2022-03-01 18:14:36 -0600

    Update metrics.py with get_balanced_metrics

--- metrics.py
+++ metrics.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from sklearn.metrics import balanced_accuracy_score, f1_score, cohen_kappa_score, matthews_corrcoef
 
 def get_mrr(predictions_dict):
     ranks = np.asarray([get_rank(value) for key, value in predictions_dict.items()])
@@ -13,3 +14,12 @@ def get_rank(dict_value):
     rank_i = np.squeeze(np.where(idx == label)) + 1
 
     return rank_i
+
+def get_balanced_metrics(output, target):
+    #Metrics for imbalanced datasets by sklearn
+    pred = torch.argmax(outputs, dim=1)
+    balanced_acc = balanced_accuracy_score(target, pred)
+    f1 = f1_score(target, pred, average='weighted')
+    cohen_kappa = cohen_kappa_score(target, pred)
+    matthews = matthews_corrcoef(target, pred)
+    return balanced_acc, f1, cohen_kappa, matthews
