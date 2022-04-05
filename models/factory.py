503c4b10537a617155c98c36049750d6c5e98acd
Kelvin Jimenez <kelvin@kelvin.cr>
2022-04-06 04:54:07 +0700

    Create Registry and Factory for model creation.

---
+++ models/factory.py
@@ -0,0 +1,32 @@
+import os
+
+from .registry import model_entrypoint, is_model
+
+
+def create_model(
+        model_name: str,
+        pretrained: bool = False,
+        checkpoint_path='',
+        **kwargs
+        ):
+    """Create a model
+    Args:
+        model_name (str): name of model to instantiate
+        pretrained (bool): load pretrained ImageNet-1k weights if true
+        checkpoint_path (str): path of checkpoint to load after model is initialized
+    Keyword Args:
+        **: other kwargs are model specific
+    """
+
+    kwargs = {k: v for k, v in kwargs.items() if v is not None and k != 'name'}
+
+    if not is_model(model_name):
+        raise RuntimeError('Unknown model (%s)' % model_name)
+
+    create_fn = model_entrypoint(model_name)
+    model = create_fn(pretrained=pretrained, **kwargs)
+
+    if os.path.isfile(checkpoint_path):
+        model.load_checkpoint(model, checkpoint_path)
+
+    return model
